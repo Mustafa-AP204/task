41afa953b59d52ef27d68f24b5489f8de266b989
User <tu7dsvbpa@code.edu.az>
2022-03-05 13:08:41 +0400

    added Program.cs

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = { "baku", "qebele", "sheki", "qax", "shamaxi" };
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length>5)
+                {
+                    Console.WriteLine(words[i]);
+                }
+
+
+                    
+            }
+
+
+            
+        }
+    }
+}
